f5fc4cc0b69cefe1f54f40b5a0fe3f0b29909f91
Kyle Ladd <kladd6@gmail.com>
2016-05-15 18:38:00 -0400

    Initial Commit

---
+++ TextBeltClient/Textbelt.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextbeltClient
+{
+    //public class TextBelt
+    //{
+    //    public string number { get; set; }
+    //    public string message { get; set; }
+
+    //    public TextBelt(string number,string message) {
+    //        this.number = number;
+    //        this.message = message;
+    //    }
+    //    public bool sendMessage(){
+            
+    //        return false;
+    //    }
+    //}
+    public class Textbelt
+    {
+        public static bool sendMessage(string number, string message)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            var obj = new { number = number, message = message };
+            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(obj));
+            HttpWebRequest request = WebRequest.Create("http://textbelt.com/text") as HttpWebRequest;
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.ContentLength = data.Length;
+            try
+            {
+                request.GetRequestStream().Write(data, 0, data.Length);
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    var rawJson = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    dynamic jsonResponse = JObject.Parse(rawJson);
+                    return Convert.ToBoolean(jsonResponse.success);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return false;
+        }
+    }
+}
---
+++ TextbeltExample/TextbeltExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TextbeltClient;
+
+namespace TextbeltExample
+{
+    public class TextbeltExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Textbelt.sendMessage("5555555555", "This is a test message").ToString());
+            Console.Read();
+        }
+    }
+}
